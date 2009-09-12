894c9642b22e5d4475d7739da29acacd217f0826
Dell Sala <dell@dellsala.com>
2009-09-12 22:57:58 +0000

    * ESys_Admin_CredentialsChecker:     - a simple credentials checker that can be configured       with a single username and password

---
+++ lib/components/ESys/Admin/CredentialsChecker.php
@@ -0,0 +1,31 @@
+<?php
+
+class ESys_Admin_CredentialsChecker implements ESys_Authenticator_CredentialsChecker {
+
+
+    public function __construct ($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+
+    public function checkCredentials ($credentials)
+    {
+        $credentials = new ESys_ArrayAccessor($credentials);
+        $credentials = $credentials->get(array(
+            'username',
+            'password',
+        ));
+        return ($this->username == $credentials['username']
+            && $this->password == $credentials['password']);
+    }
+
+
+    public function checkAuthorization ($loginId, $authorizationId)
+    {
+        return ! empty($loginId);
+    }
+
+
+}
