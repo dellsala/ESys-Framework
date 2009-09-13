d9264c78356e1771d550fed6ad0109ba30c802dc
Dell Sala <dell@dellsala.com>
2009-09-13 16:25:37 +0000

    * ESys_Scaffolding_Entity:     - updated form and list views to use markup for new ESys_Admin styles

--- lib/components/ESys/Scaffolding/Entity/templates/form-view.tpl.php
+++ lib/components/ESys/Scaffolding/Entity/templates/form-view.tpl.php
@@ -35,81 +35,94 @@ if ($form->hasErrors()) {
 
 echo $messageView->fetch();
 </php>
-<form action="<php> echo esc_html($request->url('controller').'/save'); </php>" method="post">
-<div>
-<input type="hidden" name="id" value="<php> echo esc_html($formData['id']); </php>">
-<table class="form_layout">
-<?php foreach ($attributeList as $attribute) : ?>
-    <tr>
-        <td class="label"><?php echo $attribute->displayName(); ?></td>
-<?php   switch ($attribute->type()) : ?>
-<?php
-            case ('TEXT') :
-            case ('BLOB') :
+
+<div class="center"><div class="content_block">
+
+<form class="form" action="<php> 
+    echo esc_html($request->url('controller').'/save'); </php>" method="post"><div>
+    <input type="hidden" name="id" value="<php> echo esc_html($formData['id']); </php>">
+    <table cellspacing="0">
+        <tr>
+            <th>&nbsp;</th>
+            <th>
+                <div class="content_title">
+                    <h2><?php echo $entity->displayName(); ?> Details</h2>
+                </div>
+            </th>
+        </tr>
+<?php 
+foreach ($attributeList as $attribute) : 
 ?>
-        <td><php> $formBuilder->textarea('<?php echo $attribute->name(); ?>', 
-            array('cols'=>'40', 'rows'=>'15')); </php></td>
-<?php
-            break;
-            case ('ENUM') :
-                $typeInfo = $attribute->typeInfo();
-                $enumValues = $typeInfo['spec'];
-                if ($attribute->isBoolean()) :
+        <tr>
+            <td><label class="horizontal_label"<?php if ($attribute->type() !== 'ENUM') : ?> for="<?php 
+                echo $attribute->name(); ?>"<?php endif; ?>><?php 
+                echo $attribute->displayName(); ?></label></td>
+<?php   
+    switch ($attribute->type()) : 
+        case ('TEXT') :
+        case ('BLOB') :
 ?>
-        <td>
-            <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', 'Y'); </php>Yes &nbsp;
-            <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', 'N'); </php>No
-        </td>
+            <td><div class="field"><php> $formBuilder->textarea('<?php echo $attribute->name(); ?>', 
+                array('id'=>'<?php echo $attribute->name(); ?>', 'cols'=>'40', 'rows'=>'15')); </php></div></td>
 <?php
-                else :
+        break;
+        case ('ENUM') :
+            $typeInfo = $attribute->typeInfo();
+            $enumValues = $typeInfo['spec'];
+            if ($attribute->isBoolean()) :
 ?>
-        <td>
+            <td><div class="field">
+                <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', 'Y', array('id'=>'<?php 
+                    echo $attribute->name(); ?>'); </php>Yes &nbsp;
+                <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', 'N'); </php>No
+            </div></td>
 <?php
-                    $enumValueList = explode("','", trim($enumValues, "'"));
-                    array_walk($enumValueList, create_function('&$v', '$v = str_replace("\'\'", "\'", $v);'));
-                    foreach ($enumValueList as $enumValue) : 
+            else :
 ?>
-            <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', '<?php 
-                echo addslashes($enumValue); ?>'); </php> <?php echo esc_html($enumValue); ?><br>
+            <td><div class="field">
+<?php
+                $enumValueList = explode("','", trim($enumValues, "'"));
+                array_walk($enumValueList, create_function('&$v', '$v = str_replace("\'\'", "\'", $v);'));
+                foreach ($enumValueList as $enumValue) : 
+?>
+                <php> $formBuilder->radio('<?php echo $attribute->name(); ?>', '<?php 
+                    echo addslashes($enumValue); ?>'); </php> <?php echo esc_html($enumValue); ?><br>
 <?php               
-                    endforeach; 
+                endforeach; 
 ?>
-        </td>
+            </div></td>
 <?php
-                endif;
+            endif;
 ?>
 <?php
-            break;
-            case ('DATE') :
+        break;
+        case ('DATE') :
+        case ('VARCHAR') :
+        case ('CHAR') :
+        case ('INT') :
+        default :
 ?>
-        <td>
-<php> 
-                $date = new DateTime($formData['<?php echo $attribute->name(); ?>']);
-                $dateInputView = new ESys_Template('ESys/Core/templates/date-input.tpl.php');
-                $dateInputView->set('date', $date);
-                $dateInputView->set('fieldName', '<?php echo $attribute->name(); ?>');
-                echo $dateInputView->fetch();
-</php>
-        </td>
-<?php
-            break;
-            case ('VARCHAR') :
-            case ('CHAR') :
-            case ('INT') :
-            default :
+            <td><div class="field">
+                <php> $formBuilder->input('<?php 
+                    echo $attribute->name(); ?>', array('class'=>'text', 'id'=>'<?php 
+                    echo $attribute->name(); ?>')); </php> 
+            </div></td>
+<?php       
+        break; 
+    endswitch; 
 ?>
-        <td><php> $formBuilder->input('<?php echo $attribute->name(); ?>', array('size'=>'40')); </php></td>
-<?php       break; ?>
-<?php   endswitch; ?>
-    </tr>
-<?php endforeach; ?>
-    <tr>
-        <td>&nbsp;</td>
-        <td>
-            <input type="submit" value="Save">
-            or <a href="<php> echo esc_html($request->url('controller').'/'); </php>">cancel</a>
-        </td>
-    </tr>
-</table>
-</div>
-</form>
+        </tr>
+<?php 
+endforeach; 
+?>
+        <tr>
+            <td>&nbsp;</td>
+            <td class="actions">
+                    <span class="button"><input type="submit" value="Save"></span>
+                    or <a href="<php> echo esc_html($request->url('controller').'/'); </php>" class="secondary_action">cancel</a>
+            </td>
+        </tr>
+    </table>
+</div></form>
+
+</div></div>
--- lib/components/ESys/Scaffolding/Entity/templates/list-view.tpl.php
+++ lib/components/ESys/Scaffolding/Entity/templates/list-view.tpl.php
@@ -24,28 +24,40 @@ $urlBase = $request->url('base');
         form.submit();
     }
 </script>
-<div style="margin-bottom: 1em; text-align: right;">
+<form name="deleteForm" action="<php> 
+    echo esc_html($request->url('controller').'/delete'); </php>" method="post">
+    <div><input type="hidden" name="id" value=""></div>
+</form>
+
+<div class="center"><div class="content_block">
+
+<span class="button" style="float:right;">
     <input type="button" value="New <?php 
         echo $entity->displayName(); ?>" onclick="location = '<php>
         echo $request->url('controller').'/new'; </php>';">
+</span>
+
+<div class="content_title">
+    <h2><?php echo $entity->displayName(); ?> List</h2>
+    <p>Create, update and delete <?php echo $entity->displayName(); ?> records.</p>
 </div>
-<table cellspacing="0" class="item_list">
+
+<table cellspacing="0" class="record_list">
     <tr>
-        <th>&nbsp;</th>
+        <th style="width:50px;">&nbsp;</th>
         <th><?php echo $primaryAttribute->displayName(); ?></th>
     </tr>
-<php> foreach ($<?php echo $entity->instanceName(); ?>List as $i => $<?php 
-    echo $entity->instanceName(); ?>) : </php>
-<php>
-        $editRequest = $request->url('controller').'/edit/'.$<?php echo $entity->instanceName(); ?>->get('id');
+<php> 
+foreach ($<?php echo $entity->instanceName(); ?>List as $i => $<?php echo $entity->instanceName(); ?>) : 
+    $editRequest = $request->url('controller').'/edit/'.$<?php echo $entity->instanceName(); ?>->getId();
 </php>
-    <tr class="item <php> echo ($i % 2) ? 'even' : 'odd'; </php>">
-        <td class="buttons">
+    <tr class="<php> echo ($i % 2) ? 'even' : 'odd'; </php>">
+        <td class="actions">
             <a href="#" class="delete"  onclick="requestDelete(<php> echo $<?php 
-                echo $entity->instanceName(); ?>->get('id'); </php>, '<php> 
+                echo $entity->instanceName(); ?>->getId(); </php>, '<php> 
                 echo addslashes($<?php echo $entity->instanceName(); ?>->get('<?php 
                 echo $primaryAttribute->name(); ?>')); </php>'); return false;" title="Delete">
-                <img src="<php> echo $urlBase; </php>/style/ESys/Core/images/trash.gif" alt="Trash">
+                <img src="<php> echo $urlBase; </php>/style/ESys/Admin/images/trash.gif" alt="Trash">
             </a>
             <a class="edit" href="<php> echo esc_html($editRequest); </php>" title="Edit">Edit</a>
         </td>
@@ -54,8 +66,9 @@ $urlBase = $request->url('base');
                 echo $primaryAttribute->name(); ?>')); </php></h3>
         </td>
     </tr>
-<php> endforeach; </php>
+<php> 
+endforeach; 
+</php>
 </table>
-<form name="deleteForm" action="<php> echo esc_html($request->url('controller').'/delete'); </php>" method="post">
-    <div><input type="hidden" name="id" value=""></div>
-</form>
+
+</div></div>
