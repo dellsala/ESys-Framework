945711ae55cf99931252c25cf5da03988976b784
Dell Sala <dell@dellsala.com>
2010-04-23 12:04:08 +0000

    ESys_Admin_Bootstrap: sets up an autoloader, registers a dataStoreFactory

--- lib/components/ESys/Admin/Bootstrap.php
+++ lib/components/ESys/Admin/Bootstrap.php
@@ -17,8 +17,8 @@ class ESys_Admin_Bootstrap {
         ESys_Bootstrap::init();
         ESys_Bootstrap::initDatabaseConnection();
         ESys_Bootstrap::initSession(strtoupper($packageName));
+        ESys_Bootstrap::initAutoLoader();
 
-        require_once 'ESys/Admin/CredentialsChecker.php';
         $conf = ESys_Application::get('config');
 
         ESys_Bootstrap::initAuthenticator(
@@ -27,6 +27,16 @@ class ESys_Admin_Bootstrap {
                 $conf->get($packageName, 'password')
             )
         );
+
+        $packageNameParts = explode('_', $packageName);
+        array_pop($packageNameParts);
+        array_push($packageNameParts, 'Domain');
+        $domainPackageName = implode('_', $packageNameParts);
+        ESys_Application::set(
+            'dataStoreFactory',
+            new ESys_Data_Store_Factory($domainPackageName)
+        );
+
     }
 
 
