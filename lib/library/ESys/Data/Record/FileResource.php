ae17d8285c6881a8ab701a7f39870c61505198c2
Dell Sala <dell@dellsala.com>
2011-10-05 21:59:36 -0400

    ESys_Data_Record_FileResource: - stores and manages a file within a targeted directory - useful for managing files attached to other data - unit tests

---
+++ lib/library/ESys/Data/Record/FileResource.php
@@ -0,0 +1,163 @@
+<?php
+
+class ESys_Data_Record_FileResource {
+
+
+    protected $urlDirectory;
+    
+    protected $fileSystemPathDirectory;
+
+    protected $directoryDepth;
+
+
+    public function __construct ($fileSystemPathDirectory, $urlDirectory = "", $directoryDepth = 0)
+    {
+        $this->urlDirectory = $urlDirectory;
+        $this->fileSystemPathDirectory = $fileSystemPathDirectory;
+        $this->directoryDepth = $directoryDepth;
+    }
+
+
+    public function isInstalled ()
+    {
+        if (! file_exists($this->fileSystemPathDirectory)) {
+            return false;
+        }
+        return ! $this->isFileSystemPathDirectoryEmpty();
+    }
+
+
+    public function install ($sourceFileSystemPath, $filename)
+    {
+        if (! file_exists($sourceFileSystemPath)) {
+            trigger_error(__CLASS__.'::'.__FUNCTION__."() source file ".
+                "'{$sourceFileSystemPath}' not found.", E_USER_WARNING);
+            return false;
+        }
+        $this->uninstall();
+        $this->prepareFileSystemPathDirectory();
+        if (! is_writable($this->fileSystemPathDirectory)) {
+            trigger_error(__CLASS__.'::'.__FUNCTION__."() target path ".
+                "'{$this->fileSystemPathDirectory}' is not writable.", E_USER_WARNING);
+            return false;
+        }
+        $targetFileSystemPath = $this->fileSystemPathDirectory.'/'.$filename;
+        if (! rename($sourceFileSystemPath, $targetFileSystemPath)) {
+            trigger_error(__CLASS__.'::'.__FUNCTION__."() saving '{$targetFileSystemPath}' ".
+                "failed unexpectedly.", E_USER_WARNING);
+            return false;
+        }
+        chmod($targetFileSystemPath, 0666);
+        return true;
+    }
+
+
+    public function uninstall ()
+    {
+        if (! file_exists($this->fileSystemPathDirectory)) {
+            return $this->removeFileSystemPathDirectory();
+        }
+        foreach (glob($this->fileSystemPathDirectory.'/*') as $file) {
+            if (! unlink($file)) {
+                trigger_error(__CLASS__.'::'.__FUNCTION__."() deleting '{$file}' ".
+                    "failed unexpectedly.", E_USER_WARNING);
+                return false;
+            }
+        }
+        return $this->removeFileSystemPathDirectory();
+    }
+
+
+    public function fileSystemPath ()
+    {
+        if (! $filename = $this->filename()) {
+            return null;
+        }
+        return $this->fileSystemPathDirectory.'/'.$filename;
+    }
+
+
+    public function url ()
+    {
+        if (! $filename = $this->filename()) {
+            return null;
+        }
+        return $this->urlDirectory.'/'.$filename;
+    }
+
+
+    public function filename ()
+    {
+        $fileList = glob($this->fileSystemPathDirectory.'/*');
+        if (empty($fileList)) {
+            return null;
+        }
+        return basename($fileList[0]);
+    }
+
+
+    protected function prepareFileSystemPathDirectory ()
+    {
+        return $this->prepareFileSystemPathDirectoryToDepth($this->directoryDepth);
+    }
+
+
+    protected function prepareFileSystemPathDirectoryToDepth ($depth)
+    {
+        $targetPath = $this->fileSystemPathDirectory;
+        for ($i = $depth; $i >= 0; $i--) {
+            $targetSubDir = $targetPath;
+            for ($j=0; $j < $i; $j++) {
+                $targetSubDir = dirname($targetSubDir);
+            }
+            if (! file_exists($targetSubDir)) {
+                mkdir($targetSubDir);
+                chmod($targetSubDir, 0777);
+            }
+        }
+    }
+
+
+    protected function removeFileSystemPathDirectory ()
+    {
+        return $this->removeFileSystemPathDirectoryToDepth($this->directoryDepth);
+    }
+
+
+    protected function removeFileSystemPathDirectoryToDepth ($depth)
+    {
+        $targetPath = $this->fileSystemPathDirectory;
+        for ($i = 0; $i <= $depth; $i++) {
+            $targetSubDir = $targetPath;
+            for ($j=0; $j < $i; $j++) {
+                $targetSubDir = dirname($targetSubDir);
+            }
+            if (! file_exists($targetSubDir)) {
+                continue;
+            }
+            if (count(glob($targetSubDir.'/*'))) {
+                continue;
+            }
+            rmdir($targetSubDir);
+        }
+        return true;
+    }
+
+
+    protected function isFileSystemPathDirectoryEmpty ()
+    {
+        $fileList = array ();
+        if ( $handle = opendir ( $this->fileSystemPathDirectory ) ) {
+            while ( false !== ( $file = readdir ( $handle ) ) ) {
+                if (substr($file, 0, 1) != ".") {
+                    $fileList [] = $file;
+                }
+            }
+            closedir ( $handle );
+        }
+        return (count ($fileList)) ? false : true;
+    }
+
+
+
+}
---
+++ test/ESys/Data/Record/FileResourceTest.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once 'ESys/Data/Record/FileResource.php';
+
+class ESys_Data_Record_FileResourceTest extends PHPUnit_Framework_TestCase {
+
+
+    protected $storageDir; 
+    protected $sourceFile; 
+
+
+    public function setup ()
+    {
+        $this->sourceFile = dirname(__FILE__).'/test-file.txt';
+        file_put_contents($this->sourceFile, 'foo bar');
+        $this->storageDir = dirname(__FILE__).'/storage';
+        $this->deleteDirectory($this->storageDir);
+        mkdir($this->storageDir);
+    }
+
+
+    public function teardown ()
+    {
+        if (file_exists($this->sourceFile)) {
+            unlink($this->sourceFile);
+        }
+        $this->deleteDirectory($this->storageDir);
+    }
+
+
+    public function testInstallsFile ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $this->assertTrue(
+            $fileResource->install($this->sourceFile, basename($this->sourceFile)),
+            'Install returns success result.'
+        );
+        $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
+        $this->assertTrue(
+            file_exists($expectedFileLocation),
+            'File exists in expected location.'
+        );
+    }
+
+
+    public function testDetectsIfFileIsInstalled ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
+        $this->assertFalse(
+            $fileResource->isInstalled(),
+            'Reports file not installed.'
+        );
+        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $this->assertTrue(
+            $fileResource->isInstalled(),
+            'Reports file installed.'
+        );
+        $fileResource->uninstall();
+        $this->assertFalse(
+            $fileResource->isInstalled(),
+            'Reports file not installed.'
+        );
+    }
+
+
+    public function testUninstallsFile ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
+        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $this->assertTrue(
+            $fileResource->uninstall(),
+            'Uninstall returns success result.'
+        );
+        $this->assertFalse(
+            file_exists($expectedFileLocation),
+            'File does not exist after uninstall.'
+        );
+    }
+
+
+    public function testProvidesFileSystemPath ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
+        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $this->assertEquals($expectedFileLocation, $fileResource->fileSystemPath());
+    }
+
+
+    public function testProvidesUrl ()
+    {
+        $urlBase = '/subdir';
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir, $urlBase);
+        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $expectedUrl = $urlBase.'/'.basename($this->sourceFile);
+        $this->assertEquals($expectedUrl, $fileResource->url());
+    }
+
+
+    public function testProvidesFilename ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $expectedFileName = basename($this->sourceFile);
+        $this->assertEquals($expectedFileName, $fileResource->filename());
+    }
+
+
+    protected function createSourceFile ()
+    {
+        $this->sourceFile = dirname(__FILE__).'/test-file.txt';
+        file_put_contents($this->sourceFile, 'foo bar');
+    }
+
+
+    protected function deleteSourceFile ()
+    {
+        if (file_exists($this->sourceFile)) {
+            unlink($this->sourceFile);
+        }
+    }
+
+    
+    protected function deleteDirectory ($dir)
+    {
+        if (! file_exists($dir)) {
+            return;
+        }
+        $directoryIterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($directoryIterator as $item) {
+            $item->isDir() ? rmdir($item) : unlink($item);
+        }
+        rmdir($dir);
+    }
+
+
+
+}
