59ce2e6efaca65e52e7453372d1df8e28efabc41
Dell Sala <dell@dellsala.com>
2011-10-05 22:28:10 -0400

    ESys_Data_Record_FileResource: - tests updated reflect optional target filename arg to install() - additional tests added

--- test/ESys/Data/Record/FileResourceTest.php
+++ test/ESys/Data/Record/FileResourceTest.php
@@ -33,10 +33,26 @@ class ESys_Data_Record_FileResourceTest extends PHPUnit_Framework_TestCase {
     {
         $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
         $this->assertTrue(
-            $fileResource->install($this->sourceFile, basename($this->sourceFile)),
+            $fileResource->install($this->sourceFile),
             'Install returns success result.'
         );
         $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
+        $this->assertTrue(
+            file_exists($expectedFileLocation),
+            'File exists in expected location.'
+        );
+    }
+
+
+    public function testInstallsFileWithAlternateName ()
+    {
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $alternateName = 'some-other-name.txt';
+        $this->assertTrue(
+            $fileResource->install($this->sourceFile, $alternateName),
+            'Install returns success result.'
+        );
+        $expectedFileLocation = $this->storageDir.'/'.$alternateName;
         $this->assertTrue(
             file_exists($expectedFileLocation),
             'File exists in expected location.'
@@ -52,7 +68,7 @@ class ESys_Data_Record_FileResourceTest extends PHPUnit_Framework_TestCase {
             $fileResource->isInstalled(),
             'Reports file not installed.'
         );
-        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $fileResource->install($this->sourceFile);
         $this->assertTrue(
             $fileResource->isInstalled(),
             'Reports file installed.'
@@ -69,7 +85,7 @@ class ESys_Data_Record_FileResourceTest extends PHPUnit_Framework_TestCase {
     {
         $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
         $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
-        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $fileResource->install($this->sourceFile);
         $this->assertTrue(
             $fileResource->uninstall(),
             'Uninstall returns success result.'
@@ -85,25 +101,61 @@ class ESys_Data_Record_FileResourceTest extends PHPUnit_Framework_TestCase {
     {
         $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
         $expectedFileLocation = $this->storageDir.'/'.basename($this->sourceFile);
-        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $fileResource->install($this->sourceFile);
         $this->assertEquals($expectedFileLocation, $fileResource->fileSystemPath());
     }
 
 
     public function testProvidesUrl ()
     {
-        $urlBase = '/subdir';
-        $fileResource = new ESys_Data_Record_FileResource($this->storageDir, $urlBase);
-        $fileResource->install($this->sourceFile, basename($this->sourceFile));
-        $expectedUrl = $urlBase.'/'.basename($this->sourceFile);
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
+        $fileResource->install($this->sourceFile);
+        $expectedUrl = '/'.basename($this->sourceFile);
         $this->assertEquals($expectedUrl, $fileResource->url());
     }
 
 
+    public function testProvidesUrlWithSpecifiedBase ()
+    {
+        $baseUrl = '/my/dir';
+        $fileResource = new ESys_Data_Record_FileResource($this->storageDir, $baseUrl);
+        $fileResource->install($this->sourceFile);
+        $expectedUrl = $baseUrl.'/'.basename($this->sourceFile);
+        $this->assertEquals($expectedUrl, $fileResource->url());
+    }
+
+
+    public function testManagesDirectoriesToSpecifiedDepth ()
+    {
+        $managedDirectoryDepth = 1;
+        $deepStorageDir = $this->storageDir.'/subdir';
+        $fileResource = new ESys_Data_Record_FileResource($deepStorageDir, "", $managedDirectoryDepth);
+        $expectedFileLocation = $deepStorageDir.'/'.basename($this->sourceFile);
+        
+        $this->assertTrue(
+            file_exists(dirname(dirname($expectedFileLocation))),
+            'Base storage directory exists.'
+        );
+        $this->assertFalse(
+            file_exists(dirname($expectedFileLocation)),
+            'Managed subdir does not exist.'
+        );
+        $fileResource->install($this->sourceFile);
+        $this->assertTrue(
+            file_exists($expectedFileLocation),
+            'Installed file exists inside managed subdir.'
+        );
+        $fileResource->uninstall();
+        $this->assertFalse(
+            file_exists(dirname($expectedFileLocation)),
+            'Managed subdir does not exist after unintall.'
+        );
+    }
+
     public function testProvidesFilename ()
     {
         $fileResource = new ESys_Data_Record_FileResource($this->storageDir);
-        $fileResource->install($this->sourceFile, basename($this->sourceFile));
+        $fileResource->install($this->sourceFile);
         $expectedFileName = basename($this->sourceFile);
         $this->assertEquals($expectedFileName, $fileResource->filename());
     }
