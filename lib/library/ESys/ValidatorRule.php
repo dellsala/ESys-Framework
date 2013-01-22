f188131d309b46aa36d9443dd623795c3ff722ee
Dell Sala <dellsala@avtech.com>
2013-01-22 16:33:59 -0500

    ESys_ValidatorRule_NotEmpty: new validator for enforcing non-empty values. (fails on empty string, whitespace strings, and null)

--- lib/library/ESys/ValidatorRule.php
+++ lib/library/ESys/ValidatorRule.php
@@ -471,4 +471,17 @@ class ESys_ValidatorRule_SuccessfulFileUpload extends ESys_ValidatorRule
 }
 
 
+class ESys_ValidatorRule_NotEmpty extends ESys_ValidatorRule {
+	
+	
+	public function validate ($value)
+	{
+		if (is_string($value) && preg_match('/^\s+$/', $value)) {
+			return false;
+		}
+		return strlen($value) > 0;
+	}
+	
+	
+}
 
